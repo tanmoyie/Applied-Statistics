75b080b1d93d97f4f52e760205a7a028f9738dcd
tanmoyie <tanmied@gmail.com>
2018-07-21 22:39:12 +0600

    .

---
+++ Projects & Dataset/Python Tutorials for Beginner/importing_excel_file_in_Python.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 20 00:37:43 2018
+
+@author: TANMOY
+"""
+
+"""
+importing excel files
+""" 
+import pandas
+# load the EXCEL file & read the data 
+dataframe1 = pandas.read_excel(open('Data of Performance of students in Operations Research.xlsx','rb'),
+                       sheetname=0)
+# no header
+dataframe_no_header = pandas.read_excel(open('Data of Performance of students in Operations Research.xlsx','rb'),
+                       sheetname=0, header=None)
+# if the excel file contains multiple sheet
+dataframe_2nd_sheet = pandas.read_excel(open('3D76FB00.xlsx','rb'),
+                       sheetname=1)
+    
+# convert the dataframe into an array/matrix
+input_data = dataframe1.values
