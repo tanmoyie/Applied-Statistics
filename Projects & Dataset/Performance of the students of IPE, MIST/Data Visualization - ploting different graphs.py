897ac1ec55c965aba861fa0c9bbdfd1e1c29d955
tanmoyie <tanmied@gmail.com>
2018-07-29 23:23:22 +0600

    .

--- Projects & Dataset/Performance of the students of IPE, MIST/Data Visualization - ploting different graphs.py
+++ Projects & Dataset/Performance of the students of IPE, MIST/Data Visualization - ploting different graphs.py
@@ -12,17 +12,26 @@ import matplotlib.pyplot as plt
 """
 importing CSV files
 """ 
-import pandas
-dataframe_csv = pandas.read_csv("Grading of the students in the exam (OR).csv")
-dataframe_csv.dtypes
+import pandas as pd
+dataframe_raw = pd.read_csv("Grading of the students in the exam (IPE101) raw.csv", header=0).drop_duplicates()
+dataframe_raw.dtypes
 # convert the dataframe into an array/matrix
-input_data = dataframe_csv.values
 
+# finding NaN values
+print(dataframe_raw.isna)
+df_droping_na = dataframe_raw.dropna()
+#df_drop_txt = df_droping_na[~df_droping_na['CT-4 (Marks: 20)'].isin(['Ma'])]
 
+input_data = df_droping_na.values
+df_droping_na['CT-4 (Marks: 20)'].astype(str).astype(float)
+type(df_droping_na)
+df_droping_na.dtypes
+df_matrix = df_droping_na.as_matrix
 """
 EDA
 """
 x = input_data[:,7]
-y = input_data[:,12]
+y = input_data[:,10]
 plt.scatter(x,y)
 plt.show()
+
